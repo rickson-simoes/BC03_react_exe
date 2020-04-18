dee06bc8a4c1f75a81a38dcc1516be45c17b2383
Rickson <rickson.simoes@hotmail.com>
2020-04-18 15:53:00 -0300

    :sparkles: Finalizado - fazendo testes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepos(response.data);
+    })
+  }, []);
+
+
+  async function handleAddRepository() { 
+    const addRepos = await api.post('repositories', {
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"]
+    });
+
+   return setRepos([...repos, addRepos.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    await api.get('repositories').then(response => {
+      setRepos(response.data);
+    });
+  }
+
+  async function handleAddLike(id) {
+    await api.post(`repositories/${id}/like`);
+
+    await api.get('repositories').then(response => {
+      setRepos(response.data);
+    });
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repos.map( rep => (
+          <li key={rep.id}>
+            {rep.title} <br/>
+            {rep.url} <br />
+            {rep.techs.map(f => f+' ' )}<br />
+            {rep.likes} <br />
+          
+            <button onClick={() => handleRemoveRepository(rep.id)}>
+              Remover
+            </button>
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleAddLike(rep.id)}>
+              Add Like
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
